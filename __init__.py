21b8f439985b2b21551ba60cd3f62deca62b0426
?ngel ?lvarez <angel@nan-tic.com>
2019-01-04 11:23:50 +0100

    add compatibility with sale_supply_production to avoid duplicated code

--- __init__.py
+++ __init__.py
@@ -1,23 +1,14 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
+
 from trytond.pool import Pool
-from . import production
-from . import sale
+from .production import *
+from .sale import *
+
 
 def register():
     Pool.register(
-        production.Production,
-        sale.Plan,
-        sale.SaleLine,
-        sale.Sale,
+        Production,
+        Plan,
+        SaleLine,
         module='sale_cost_plan', type_='model')
-    Pool.register(
-        production.ChangeQuantityStart,
-        sale.ChangeLineQuantityStart,
-        depends=['sale_change_quantity'],
-        module='sale_cost_plan', type_='model')
-    Pool.register(
-        production.ChangeQuantity,
-        sale.ChangeLineQuantity,
-        depends=['sale_change_quantity'],
-        module='sale_cost_plan', type_='wizard')
--- tests/scenario_sale_cost_plan.rst
+++ tests/scenario_sale_cost_plan.rst
@@ -230,11 +230,4 @@ Sale product with first plan::
     >>> sale.state
     'processing'
     >>> sale.reload()
-    >>> len(sale.productions) == 1
-    True
-    >>> production, = sale.productions
-    >>> production.product == product
-    True
-    >>> production.quantity == 2.0
-    True
-
+    
